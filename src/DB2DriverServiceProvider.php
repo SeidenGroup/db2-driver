2c0efdefed2484de8bd2508bbce87ff7fff2c1a3
Calvin Buckley <calvin@cmpct.info>
2022-12-21 11:34:11 -0400

    This is on , not the Package

--- src/DB2DriverServiceProvider.php
+++ src/DB2DriverServiceProvider.php
@@ -9,8 +9,9 @@ class DB2DriverServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        $package->name('db2-driver')
-            ->publishes([
+        $package->name('db2-driver');
+        // Not a fluent thing, but operates on self. Tortured construct to be compatible with 1.12
+        $this->publishes([
                 $this->package->basePath("/../resources/stubs/DB2ServiceProvider.php") => base_path("app/Providers/DB2ServiceProvider.php"),
             ], "{$this->package->shortName()}-provider");
     }
