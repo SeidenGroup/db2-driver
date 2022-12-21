81c720039d8263a5fdce60d92822e2de5b5a9f8f
Calvin Buckley <calvin@cmpct.info>
2022-12-21 11:32:03 -0400

    WIP: Try using functionality available in 1.12 of package tools

--- src/DB2DriverServiceProvider.php
+++ src/DB2DriverServiceProvider.php
@@ -10,6 +10,8 @@ class DB2DriverServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name('db2-driver')
-            ->publishesServiceProvider('DB2ServiceProvider');
+            ->publishes([
+                $this->package->basePath("/../resources/stubs/DB2ServiceProvider.php") => base_path("app/Providers/DB2ServiceProvider.php"),
+            ], "{$this->package->shortName()}-provider");
     }
 }
